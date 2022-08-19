cb01875181d9b396123e284a8c9ba202159924cb
Arnaud Panapadeatchy <arnaud.panapadeatchy@dsinstruments.fr>
2022-08-19 16:58:53 +0400

    real initial commit

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -10,7 +10,7 @@ import NotificationsOutlinedIcon from '@mui/icons-material/NotificationsOutlined
 import MedicalInformationOutlinedIcon from '@mui/icons-material/MedicalInformationOutlined';
 import PsychologyOutlinedIcon from '@mui/icons-material/PsychologyOutlined';
 import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
-import PersonOutlined from '@mui/icons-material/PersonOutlined';
+import PersonOutlinedIcon from '@mui/icons-material/PersonOutlined';
 import LogoutOutlinedIcon from '@mui/icons-material/LogoutOutlined';
 
 const Sidebar = () => {
@@ -22,57 +22,65 @@ const Sidebar = () => {
       <hr />
       <div className="center">
         <ul>
+          <p className="title">Main</p>
           <li>
-            <DashboardIcon />
+            <DashboardIcon className="icon" />
             <span>Dashboard</span>
           </li>
+          <p className="title">Lists</p>
           <li>
-            <PeopleOutlineOutlinedIcon />
+            <PeopleOutlineOutlinedIcon className="icon" />
             <span>Users</span>
           </li>
           <li>
-            <StoreMallDirectoryOutlinedIcon />
+            <StoreMallDirectoryOutlinedIcon className="icon" />
             <span>Products</span>
           </li>
           <li>
-            <CreditCardOutlinedIcon />
+            <CreditCardOutlinedIcon className="icon" />
             <span>Orders</span>
           </li>
           <li>
-            <LocalShippingOutlinedIcon />
+            <LocalShippingOutlinedIcon className="icon" />
             <span>Delivery</span>
           </li>
+          <p className="title">Useful</p>
           <li>
-            <InsertChartOutlinedIcon />
+            <InsertChartOutlinedIcon className="icon" />
             <span>Stats</span>
           </li>
           <li>
-            <NotificationsOutlinedIcon />
+            <NotificationsOutlinedIcon className="icon" />
             <span>Notifications</span>
           </li>
+          <p className="title">Services</p>
           <li>
-            <MedicalInformationOutlinedIcon />
+            <MedicalInformationOutlinedIcon className="icon" />
             <span>System Health</span>
           </li>
           <li>
-            <PsychologyOutlinedIcon />
+            <PsychologyOutlinedIcon className="icon" />
             <span>Logs</span>
           </li>
           <li>
-            <SettingsOutlinedIcon />
+            <SettingsOutlinedIcon className="icon" />
             <span>Settings</span>
           </li>
+          <p className="title">User</p>
           <li>
-            <PersonOutlined />
+            <PersonOutlinedIcon className="icon" />
             <span>Profile</span>
           </li>
           <li>
-            <LogoutOutlinedIcon />
+            <LogoutOutlinedIcon className="icon" />
             <span>Logout</span>
           </li>
         </ul>
       </div>
-      <div className="bottom">color options</div>
+      <div className="bottom">
+        <div className="colorOption"></div>
+        <div className="colorOption"></div>
+      </div>
     </div>
   )
 }
