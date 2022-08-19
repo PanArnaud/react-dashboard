87a15d625242dd38f99fde6c08a941dc752d9b6a
Arnaud Panapadeatchy <a.panapadeatchy@hotmail.fr>
2022-08-20 00:10:29 +0400

    Finish homepage

---
+++ src/components/chart/Chart.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+import "./chart.scss";
+import {
+  XAxis,
+  CartesianGrid,
+  Tooltip,
+  AreaChart,
+  Area,
+  ResponsiveContainer,
+} from "recharts";
+
+const data = [
+  { name: "January", total: 1200 },
+  { name: "February", total: 2100 },
+  { name: "March", total: 800 },
+  { name: "April", total: 1600 },
+  { name: "May", total: 900 },
+  { name: "June", total: 1700 },
+];
+
+const Chart = () => {
+  return (
+    <div className="chart">
+      <div className="title">Last 6 Month (Revenue)</div>
+      <ResponsiveContainer width="100%" aspect={2 / 1}>
+        <AreaChart
+          width={730}
+          height={250}
+          data={data}
+          margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
+        >
+          <defs>
+            <linearGradient id="total" x1="0" y1="0" x2="0" y2="1">
+              <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
+              <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
+            </linearGradient>
+          </defs>
+          <XAxis dataKey="name" stroke="gray" />
+          <CartesianGrid strokeDasharray="3 3" className="chartGrid" />
+          <Tooltip />
+          <Area
+            type="monotone"
+            dataKey="total"
+            stroke="#8884d8"
+            fillOpacity={1}
+            fill="url(#total)"
+          />
+        </AreaChart>
+      </ResponsiveContainer>
+    </div>
+  );
+};
+
+export default Chart;
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,24 +1,35 @@
-import React from 'react'
-import Navbar from '../../components/navbar/Navbar'
-import Sidebar from '../../components/sidebar/Sidebar'
-import Widget from '../../components/widget/Widget'
-import './home.scss'
+import React from "react";
+import Chart from "../../components/chart/Chart";
+import Featured from "../../components/featured/Featured";
+import Navbar from "../../components/navbar/Navbar";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Table from "../../components/table/Table";
+import Widget from "../../components/widget/Widget";
+import "./home.scss";
 
 const Home = () => {
   return (
-    <div className='home'>
+    <div className="home">
       <Sidebar />
       <div className="homeContainer">
         <Navbar />
-        <div className='widgets'>
-          <Widget type='users' />
-          <Widget type='orders' />
-          <Widget type='earnings' />
-          <Widget type='balance' />
+        <div className="widgets">
+          <Widget type="users" />
+          <Widget type="orders" />
+          <Widget type="earnings" />
+          <Widget type="balance" />
+        </div>
+        <div className="charts">
+          <Featured />
+          <Chart />
+        </div>
+        <div className="listContainer">
+          <div className="listTitle">Latest Transactions</div>
+          <Table />
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
